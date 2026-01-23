9df3c886591ab1fe800f01035c84eabd6355c6d2
@Stephane4710 <lesaints969@gmail.com>
2026-01-23 18:35:27 +0100

    auto create superuser for render

--- serveur/apps.py
+++ serveur/apps.py
@@ -1,25 +1,22 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 import os
 
-
 class ServeurConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'serveur'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "serveur"
 
     def ready(self):
+        from django.contrib.auth import get_user_model
         User = get_user_model()
 
         username = os.environ.get("sk")
         email = os.environ.get("lesaints969@gmail.com")
         password = os.environ.get("Dieu4710")
 
-        if not username or not password:
-            return
-
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password
-            )
+        if username and password:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email=email,
+                    password=password
+                )
