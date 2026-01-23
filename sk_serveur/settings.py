f5588a473c243e3ec87f3bfcc126b85ee3500bd3
@Stephane4710 <lesaints969@gmail.com>
2026-01-23 18:31:50 +0100

    auto create superuser for render

--- sk_serveur/settings.py
+++ sk_serveur/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'serveur',
+    'serveur.apps.ServeurConfig',
+
 ]
 
 MIDDLEWARE = [
